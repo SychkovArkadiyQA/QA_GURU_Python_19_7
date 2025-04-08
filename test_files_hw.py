072cb1c1b8823964cec8df66402ad89efc80de6d
arkadijsyckov <Z4mmer@gmail.com>
2025-04-09 04:09:41 +0500

    Добавил архивирование в папку resources

--- test_files_hw.py
+++ test_files_hw.py
@@ -11,9 +11,23 @@ from openpyxl import load_workbook
 
 
 CURRENT_FILE = os.path.abspath(__file__) # получаем абсолютный путь к текущему файлу
-CURRENT_DIRECTORY = os.path.dirname(CURRENT_FILE  ) # получаем абсолютный путь к текущей директории
-TMP_DIR = os.path.join(CURRENT_DIRECTORY, 'files') # делаем склейку пути к текущей директории и папки files
-if not os.path.exists(TMP_DIR):
-    os.mkdir(TMP_DIR)
+CURRENT_DIRECTORY = os.path.dirname(CURRENT_FILE) # получаем абсолютный путь к текущей директории
+TMP_DIR = os.path.join(CURRENT_DIRECTORY, 'files') # делаем склейку пути к текущей директории и папке files
+ZIP_DIR = os.path.join(CURRENT_DIRECTORY, 'resources') # делаем склейку пути к текущей директории и папке resources
 
 # TODO Запаковать кодом в zip архив несколько разных файлов: pdf, xlsx, csv;
+# TODO Положить его в ресурсы;
+
+def test_create_zip():
+    file_names = ['CSV_for_test.csv',
+                  'XLSX_for_test.xlsx',
+                  'PDF_for_test.pdf']
+
+    archive_path = os.path.join(ZIP_DIR , 'archive.zip')
+
+    with zipfile.ZipFile(archive_path, 'w') as zf:
+        for file in file_names:
+            file_path = os.path.join(TMP_DIR, file)
+            zf.write(file_path, file)
+
+# TODO Реализовать чтение и проверку содержимого каждого файла из архива не распаковывая сам архив;
