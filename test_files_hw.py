e2c60f0fb9e037ca1b36c821586c2087dd959cf0
arkadijsyckov <Z4mmer@gmail.com>
2025-04-09 03:16:42 +0500

    Подготовил файлы для дз

--- test_files_hw.py
+++ test_files_hw.py
@@ -12,5 +12,8 @@ from openpyxl import load_workbook
 
 CURRENT_FILE = os.path.abspath(__file__) # получаем абсолютный путь к текущему файлу
 CURRENT_DIRECTORY = os.path.dirname(CURRENT_FILE  ) # получаем абсолютный путь к текущей директории
-# где находится файл с которым работаем
 TMP_DIR = os.path.join(CURRENT_DIRECTORY, 'files') # делаем склейку пути к текущей директории и папки files
+if not os.path.exists(TMP_DIR):
+    os.mkdir(TMP_DIR)
+
+# TODO Запаковать кодом в zip архив несколько разных файлов: pdf, xlsx, csv;
